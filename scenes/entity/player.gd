9651e4383236882df19a8b8b5b3ae8a64070db7f
nigelloppie <nigelloppie@gmail.com>
2025-11-08 14:27:47 -0700

    - initial commit

---
+++ scenes/entity/player.gd
@@ -0,0 +1,28 @@
+extends Node3D
+
+const TRAVEL_TIME := 0.3
+
+@onready var front_ray := $FrontRay
+@onready var back_ray := $BackRay
+
+var tween
+
+func _physics_process(_delta: float) -> void:
+	if tween is Tween:
+		if tween.is_running():
+			return
+	if Input.is_action_pressed("forward") and not front_ray.is_colliding():
+		tween = create_tween().set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
+		tween.tween_property(self, "transform", transform.translated_local(Vector3.FORWARD * 2), TRAVEL_TIME)
+
+	if Input.is_action_pressed("back") and not back_ray.is_colliding():
+		tween = create_tween().set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
+		tween.tween_property(self, "transform", transform.translated_local(Vector3.BACK * 2), TRAVEL_TIME)
+
+	if Input.is_action_pressed("left"):
+		tween = create_tween().set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
+		tween.tween_property(self, "transform:basis", transform.basis.rotated(Vector3.UP, PI / 2), TRAVEL_TIME)
+
+	if Input.is_action_pressed("right"):
+		tween = create_tween().set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
+		tween.tween_property(self, "transform:basis", transform.basis.rotated(Vector3.UP, -PI / 2), TRAVEL_TIME)
