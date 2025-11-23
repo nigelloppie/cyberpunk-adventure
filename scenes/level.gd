a6e03b733c6144f02dd4640067b3801d5e3d9c2f
nigelloppie <nigelloppie@gmail.com>
2025-11-23 10:47:09 -0700

    - Added lights out puzzle

--- scenes/level.gd
+++ scenes/level.gd
@@ -3,3 +3,10 @@ extends Node3D
 func _ready() -> void:
 	print("LOAD SAVE")
 	SaveData.load()
+
+func _open_secret_door():
+	print("Puzzle solved!")
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("f4"):
+		$CanvasLayer/LightsOutMenu.open_menu()
